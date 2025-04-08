63595f87e0338379ecce0fbc7f566a2a2a798d4b
Safa325 <shamarisafa@gmail.com>
2025-04-08 19:59:46 +0200

    updating files

--- videoflix/urls.py
+++ videoflix/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def healthcheck(request):
+    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('userAuth.urls')),
     path('api/', include('videos.urls')),
+    path('health', healthcheck),
 ]
 
 # Statische Medien-Dateien IMMER verfügbar machen
